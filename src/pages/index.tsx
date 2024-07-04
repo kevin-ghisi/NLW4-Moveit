b65a5e0ae10ce388345579d1130c52455cef8077
Kévin Ghisi <kevinghisi4@gmail.com>
2024-07-04 08:31:16 -0300

    First Commit, creating base app;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,21 @@
 import Head from 'next/head'
-import { GetServerSideProps } from 'next'
-
-import { CompletedChallenges } from '../components/CompletedChallenges'
-import { ExperienceBar } from '../components/ExperienceBar'
-import { ChallengeBox } from '../components/ChallengeBox'
-import { CountDown } from '../components/CountDown'
-import { Profile } from '../components/Profile'
 
 import styles from '../styles/pages/Home.module.css'
-import { CountDownProvider } from '../contexts/CountDownContext'
-import { ChallengesProvider } from '../contexts/ChallengesContext'
 
-interface HomeProps {
-  level: number;
-  currentExperience: number;
-  challengesCompleted: number;
-}
-
-export default function Home(props: HomeProps) {
+export default function Home() {
 
   return (
-    <ChallengesProvider 
-      level={props.level} 
-      currentExperience={props.currentExperience} 
-      challengesCompleted={props.challengesCompleted}
-    >
-      <div className={styles.container}>
-        <Head>
-          <title>Início | move.it</title>
-        </Head>
-
-        <ExperienceBar />
-
-        <CountDownProvider>
-          <section>
-            <div>
-              <Profile />
-              <CompletedChallenges />
-              <CountDown />
-            </div>
-            <div>
-              <ChallengeBox />
-            </div>
-          </section>
-        </CountDownProvider>
-      </div>
-    </ChallengesProvider>
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <section>
+        <div>
+          <h1>Oi</h1>
+        </div>
+        <div>
+        </div>
+      </section>
+    </div>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-
-  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
-
-
-  return {
-    props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      challengesCompleted: Number(challengesCompleted)
-    }
-  }
 }
